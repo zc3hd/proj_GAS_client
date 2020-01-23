16c37a159c11c120f4fb416ed60877865842b6d6
zc3hd <23397295+zc3hd@users.noreply.github.com>
2020-01-23 09:41:09 +0800

    upd:reload conf.js&&gas_client.js

---
+++ api_server/conf.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // 数据库名称
+  db: "gas_client",
+
+  // 后台 打包后/测试时 被代理的端口
+  api_port: 8887, // 线上
+  // api_port: 1012, // 本地
+
+
+  // 前端打包前后的根目录名
+  web_src: "src_webapp",
+  web_dist: "webapp"
+};
